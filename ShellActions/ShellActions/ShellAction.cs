e8d2afd362db497d9a5be96526d694a14c27aba6
rlovely <rlovely@localhost>
2013-06-14 13:45:03 +0100

    Adding get files in dir, print and open actions

--- ShellActions/ShellActions/ShellAction.cs
+++ ShellActions/ShellActions/ShellAction.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using CentipedeInterfaces;
 using Microsoft.VisualBasic.FileIO;
+using SearchOption = System.IO.SearchOption;
 
 
 namespace ShellActions
@@ -105,8 +106,7 @@ namespace ShellActions
         {
             string from = ParseStringForVariable(this.From);
 
-            DirectoryInfo directory;
-            directory = Path.IsPathRooted(@from) ? FileSystem.GetDirectoryInfo(@from) : Pwd;
+            DirectoryInfo directory = Path.IsPathRooted(@from) ? FileSystem.GetDirectoryInfo(@from) : Pwd;
             foreach (FileInfo file in directory.EnumerateFiles(@from))
             {
                 file.CopyTo(ParseStringForVariable(this.To), this.AllowOverWrite);
@@ -243,4 +243,80 @@ namespace ShellActions
             Variables[DestinationVar] = Path.GetDirectoryName(ParseStringForVariable(Filename));
         }
     }
+
+    [ActionCategory("Shell", DisplayName = "Get files in directory")]
+    public class GetFilesInDirectory : ShellAction
+    {
+        public GetFilesInDirectory(IDictionary<string, object> v, ICentipedeCore core)
+            : base("Get files in directory", v, core)
+        { }
+
+        [ActionArgument(Literal = true, DisplayName="Destination var")]
+        public string DestinationVariable = "";
+        
+        [ActionArgument(Literal = false)]
+        public string Pattern = "*.*";
+
+        [ActionArgument]
+        public bool Recurse = false;
+
+        
+
+        protected override void DoAction()
+        {
+            var pattern = ParseStringForVariable(Pattern);
+            FileInfo[] fileInfos = Pwd.GetFiles(pattern, this.Recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            Variables[DestinationVariable] = fileInfos.Select(fi => fi.FullName).ToList();
+        }
+    }
+
+    [ActionCategory("Shell", DisplayName = "Print file")]
+    public class PrintFile : ShellAction
+    {
+        public PrintFile(IDictionary<string, object> v, ICentipedeCore core)
+            : base("Print file", v, core)
+        { }
+
+        [ActionArgument]
+        public string Filename = "";
+        
+        protected override void DoAction()
+        {
+            Process p = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                                        {
+                                            CreateNoWindow = true,
+                                            Verb = "print",
+                                            FileName = ParseStringForVariable(this.Filename)
+                                        }
+                        };
+            p.Start();
+        }
+    }
+
+    [ActionCategory("Shell", DisplayName = "Open file")]
+    public class OpenFile : ShellAction
+    {
+        public OpenFile(IDictionary<string, object> v, ICentipedeCore core)
+            : base("Open file", v, core)
+        { }
+
+        [ActionArgument]
+        public string Filename = "";
+
+        protected override void DoAction()
+        {
+            Process p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    Verb = "open",
+                    FileName = ParseStringForVariable(this.Filename)
+                }
+            };
+            p.Start();
+        }
+    }
+
 }
