59f6f187a6ea2a863605ed03e877bace9568cd22
AMCarter <AMCarter@DERBY-15M4BT1.chemineer-derby.local>
2013-08-02 13:00:53 +0100

    Added ParseStringFor Variable to interpolated arguments. Added usage strings and action messages

--- SolidworksActions/SolidworksActions/Action.cs
+++ SolidworksActions/SolidworksActions/Action.cs
@@ -16,8 +16,7 @@ namespace SolidworksActions
 			: base(name, variables, c)
 		{ }
 
-		
-		[ActionArgument(displayName="SolidWorks Document Variable")]
+		[ActionArgument(DisplayName="SolidWorks Document Variable", Literal=true)]
 		public String SolidWorksDocVar = "SldWrksDoc";
 
 		protected ModelDoc2 SolidWorksDoc;
@@ -32,7 +31,7 @@ namespace SolidworksActions
 			SolidWorks.Visible = true;
 
 			Object obj;
-			Variables.TryGetValue( SolidWorksDocVar, out obj);
+			Variables.TryGetValue(SolidWorksDocVar, out obj);
 			SolidWorksDoc = obj as ModelDoc2;
 
 		}
@@ -84,7 +83,7 @@ namespace SolidworksActions
 
 	}
 
-	[ActionCategory("SolidWorks", iconName="solidworks")]
+	[ActionCategory("SolidWorks", IconName="solidworks")]
 	public class OpenSolidWorksFile : SolidworksAction
 	{
 		public OpenSolidWorksFile(IDictionary<string, object> v, ICentipedeCore c)
@@ -93,13 +92,17 @@ namespace SolidworksActions
 
 
 		[ActionArgument]
-		public String Filename = "";
+		public String FileName = "";
 	
 		protected override void DoAction()
-		{   
+		{
+            string myFileName = Path.GetFullPath(ParseStringForVariable(FileName));
+
+            Message("Opening file \"" + myFileName + "\"");
+
 			try
 			{
-				SolidWorksDoc = SolidWorks.OpenFile(ParseStringForVariable(Filename));
+				SolidWorksDoc = SolidWorks.OpenFile(myFileName);
 			}
 			catch (SolidWorksException e)
 			{
@@ -125,19 +128,23 @@ namespace SolidworksActions
 	//    }
 	//}
 
-	[ActionCategory("SolidWorks", displayName = "Insert Design Table", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Insert Design Table", IconName = "solidworks")]
 	public class SwInsertDesignTable : SolidworksAction
 	{
 		public SwInsertDesignTable(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Insert Design Table", v, c)
 		{ }
 		
-		[ActionArgument(displayName = "Design Table Filename")]
+		[ActionArgument(DisplayName = "Design Table Filename")]
 		public String Filename = "";
 		
 		protected override void DoAction()
 		{
-			if (!SolidWorksDoc.InsertFamilyTableOpen(Filename))
+            string myFileName = ParseStringForVariable(Filename);
+            
+            Message("Inserting design table \"" + myFileName + "\"");
+
+			if (!SolidWorksDoc.InsertFamilyTableOpen(myFileName))
 			{
 				throw new SolidWorksActionException(this);
 			}
@@ -145,70 +152,76 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Set Active Config", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Set Active Config", IconName = "solidworks", Usage="Sets the active configuration in the active document")]
 	public class SetActiveSolidWorksConfig : SolidworksAction
 	{
 		public SetActiveSolidWorksConfig(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Set Active Config", v, c)
 		{ }
 
-		[ActionArgument(displayName = "Configuration name")]
+		[ActionArgument(DisplayName = "Configuration name")]
 		public String ConfigName = "";
 
 		protected override void DoAction()
 		{
+            string myConfigName = ParseStringForVariable(ConfigName);
+
+            Message("Setting active configuration to \"" + myConfigName + "\"");
+
 			Configuration configuration = (Configuration)SolidWorksDoc.GetActiveConfiguration();
-			if (configuration.Name == ConfigName) return;
-			if (!SolidWorksDoc.ShowConfiguration2(ConfigName))
+			if (configuration.Name == myConfigName) return;
+			if (!SolidWorksDoc.ShowConfiguration2(myConfigName))
 			{
 				throw new SolidWorksActionException(this);
 			}
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Set Component Config", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Set Component Config", IconName = "solidworks", Usage="Sets the refernced configuration of a component in the active document")]
 	public class SetSwComponentConfig : SolidworksAction
 	{
 		public SetSwComponentConfig(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Set Component Config", v,c)
 		{ }
 
-		[ActionArgument(displayName = "Component Name")]
+		[ActionArgument(DisplayName = "Component Name", Usage="Enter the name of the component as shown in the feature manager (Case sensitive)")]
 		public String ComponentName = "";
 				
-		[ActionArgument(displayName = "Instance ID")]
+		[ActionArgument(DisplayName = "Instance ID", Usage="Enter the instance ID of the component as shown in the feature manager")]
 		public String InstanceId = "";
 
-		[ActionArgument(displayName = "Configuration Name")]
+		[ActionArgument(DisplayName = "Configuration Name", Usage="Enter the name of the configuration that this component should reference")]
 		public String ConfigName = "";
 
-		[ActionArgument(displayName = "Supress")]
-		public bool SuppressionState = true;
-
 		protected override void DoAction()
-		{   
+		{
+            string myComponentName = ParseStringForVariable(ComponentName);
+            string myInstanceId = ParseStringForVariable(InstanceId);
+            string myConfigName = ParseStringForVariable(ConfigName);
+
+            Message("Setting component \"" + myComponentName + ":" + myInstanceId + "\" to reference configuration \"" + myConfigName + "\"");
+
 			if (!(SolidWorksDoc is AssemblyDoc))
 			{
 				throw new SolidWorksActionException("Cannot set component config because current document is not an assembly.", this);
 			}
-			SolidWorks.SelectFeature(SolidWorksDoc, ComponentName, InstanceId, "COMPONENT");
+			SolidWorks.SelectFeature(SolidWorksDoc, myComponentName, myInstanceId, "COMPONENT");
 			var selectionMgr = SolidWorksDoc.SelectionManager as SelectionMgr;
 			if (selectionMgr != null)
 			{
 				var component = selectionMgr.GetSelectedObjectsComponent2(1) as Component2;
-				if (component != null) component.ReferencedConfiguration = ConfigName;
+				if (component != null) component.ReferencedConfiguration = myConfigName;
 			}
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Set Dimension", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Set Dimension", IconName = "solidworks", Usage="Sets the value of a dimension in the active document")]
 	public class SetSwDimension : SolidworksAction
 	{
-		[ActionArgument(displayName = "Dimension Name")]
+		[ActionArgument(DisplayName = "Dimension Name", Usage="Enter the name of the dimension to set, for example: D1@Sketch1")]
 		public String DimensionName = "";
-
-		
-		[ActionArgument(displayName = "New Value")]
+        		
+		[ActionArgument(DisplayName = "New Value", Usage="Enter the new value: linear dimensions must be specified in metres, angular dimensions in radians")]
 		public String Value = "";
 
 
@@ -218,23 +231,26 @@ namespace SolidworksActions
 
 		protected override void DoAction()
 		{
-			var dimension = SolidWorksDoc.Parameter(DimensionName) as Dimension;
+            string myDimensionName = ParseStringForVariable(DimensionName);
+            string myValue = ParseStringForVariable(Value);
+
+            Message("Setting Dimension \"" + myDimensionName + "\" to value: " + myValue);
+
+			var dimension = SolidWorksDoc.Parameter(myDimensionName) as Dimension;
 			if (dimension == null)
 			{
-				throw new SolidWorksActionException(String.Format("Dimension named {0} not found", DimensionName), this);
+				throw new SolidWorksActionException(String.Format("Dimension named {0} not found", myDimensionName), this);
 			}
 			if (dimension.ReadOnly)
 			{
-				Warning("Changed dimension {0} is read only.", DimensionName);
+				Warning("Changed dimension {0} is read only.", myDimensionName);
 			}
 
 			Double val;
 
-			string expanded = ParseStringForVariable(Value);
-
-			if (!Double.TryParse(expanded, out val))
+            if (!Double.TryParse(myValue, out val))
 			{
-				throw new SolidWorksActionException(String.Format("Invalid Value for value: {0}", Value), this);
+				throw new SolidWorksActionException(String.Format("Invalid Value for value: {0}", myValue), this);
 			}
 			if ((swSetValueReturnStatus_e)dimension.SetSystemValue2(val, (int)swSetValueInConfiguration_e.swSetValue_UseCurrentSetting) != swSetValueReturnStatus_e.swSetValue_Successful)
 			{
@@ -244,14 +260,14 @@ namespace SolidworksActions
 			{
 				Warning("Dimension update was limited to active configuration.");
 			}
-			if (!SolidWorksDoc.EditRebuild3())
-			{
-				throw new SolidWorksActionException(this);
-			}
+			//if (!SolidWorksDoc.EditRebuild3())
+			//{
+			//	throw new SolidWorksActionException(this);
+			//}
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Rebuild")]
+    [ActionCategory("SolidWorks", DisplayName = "Rebuild", IconName = "solidworks", Usage = "Performs a forced full rebuild")]
 	public class RebuildSolidWorks : SolidworksAction
 	{
 		public RebuildSolidWorks(IDictionary<String, Object> v, ICentipedeCore c)
@@ -264,26 +280,29 @@ namespace SolidworksActions
 
 		protected override void DoAction()
 		{
+
+            Message("Rebuilding");
+
 			if (!SolidWorksDoc.ForceRebuild3(TopOnly))
 			{
-				throw new SolidWorksActionException("CAnnot force rebuild", this);
+				throw new SolidWorksActionException("Cannot force rebuild", this);
 			}
 			
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Set Suppression State", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName="Set Suppression State", IconName="solidworks", Usage="Suppresses or Unsuppresses features, mates, components, patterns etc. in the active document")]
 	public class SetSwSuppressionState : SolidworksAction
 	{
 		
-		[ActionArgument(displayName="Feature Name")]
+		[ActionArgument(DisplayName="Feature Name", Usage="Enter the name of the feature as shown in the feature manager (Case sensitive)")]
 		public string FeatureName="";
 
-		[ActionArgument(displayName="Instance ID")]
+		[ActionArgument(DisplayName="Instance ID", Usage="For components of an assembly enter the instance ID, otherwise leave blank")]
 		public string InstanceId="";
 
-		[ActionArgument(displayName="Item Type")]
-		public string ItemType = "";
+		[ActionArgument(DisplayName="Item Type", Usage="See the SolidWorks API help page for \"swSelectType_e\".  This element must contain one of the valid string values")]
+		public string ItemType="";
 
 		[ActionArgument]
 		public bool Suppress = true;
@@ -294,7 +313,13 @@ namespace SolidworksActions
 
 		protected override void DoAction()
 		{
-			SolidWorks.SelectFeature(SolidWorksDoc, FeatureName, InstanceId, ItemType);
+            string myFeatureName = ParseStringForVariable(FeatureName);
+            string myInstanceId = ParseStringForVariable(InstanceId);
+            string myItemType = ParseStringForVariable(ItemType);
+
+            Message("Setting suppression state of \"" + myFeatureName + ":" + myInstanceId + "\" to " + Convert.ToString(Suppress));
+
+			SolidWorks.SelectFeature(SolidWorksDoc, myFeatureName, myInstanceId, myItemType);
 			if (Suppress)
 			{
 				SolidWorksDoc.EditSuppress();
@@ -306,25 +331,31 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Delete Item", iconName = "solidworks")]
+    [ActionCategory("SolidWorks", DisplayName = "Delete Item", IconName = "solidworks", Usage = "Deletes features, mates, components, patterns etc. in the active document")]
 	public class DeleteSwItem : SolidworksAction
 	{
 		public DeleteSwItem(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Delete Item", v, c)
 		{ }
 
-		[ActionArgument(displayName = "Feature Name")]
-		public string FeatureName = "";
+        [ActionArgument(DisplayName = "Feature Name", Usage = "Enter the name of the feature as shown in the feature manager (Case sensitive)")]
+        public string FeatureName = "";
 
-		[ActionArgument(displayName = "Instance ID")]
-		public string InstanceId = "";
+        [ActionArgument(DisplayName = "Instance ID", Usage = "For components of an assembly enter the instance ID, otherwise leave blank")]
+        public string InstanceId = "";
 
-		[ActionArgument(displayName = "Item Type")]
-		public string ItemType = "";
+        [ActionArgument(DisplayName = "Item Type", Usage = "See the SolidWorks API help page for \"swSelectType_e\".  This element must contain one of the valid string values")]
+        public string ItemType = "";
 
 		protected override void DoAction()
 		{
-			SolidWorks.SelectFeature(SolidWorksDoc, FeatureName, InstanceId, ItemType);
+            string myFeatureName = ParseStringForVariable(FeatureName);
+            string myInstanceId = ParseStringForVariable(InstanceId);
+            string myItemType = ParseStringForVariable(ItemType);
+
+            Message("Deleting item \"" + myFeatureName + ":" + myInstanceId + "\"");
+
+			SolidWorks.SelectFeature(SolidWorksDoc, myFeatureName, myInstanceId, myItemType);
 			if (!SolidWorksDoc.Extension.DeleteSelection2((int)swDeleteSelectionOptions_e.swDelete_Absorbed|(int)swDeleteSelectionOptions_e.swDelete_Children))
 			{
 				throw new SolidWorksActionException(this);
@@ -332,7 +363,7 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Delete Inactive Configurations", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Delete Inactive Configurations", IconName = "solidworks", Usage="Deletes configurations that are not used in any open documents")]
 	public class DeleteInactiveSwConfigurations : SolidworksAction
 	{
 		public DeleteInactiveSwConfigurations(IDictionary<String, Object> v, ICentipedeCore c)
@@ -341,16 +372,17 @@ namespace SolidworksActions
 
 		protected override void DoAction()
 		{
-			IEnumerable<String> configNames = (string[])SolidWorksDoc.GetConfigurationNames();
+    		IEnumerable<String> configNames = (string[])SolidWorksDoc.GetConfigurationNames();
 			string activeConfigName = SolidWorksDoc.ConfigurationManager.ActiveConfiguration.Name;
 			foreach (string name in configNames.Where(name => name != activeConfigName))
 			{
+                Message("Deleting inactive configuration: \"" + name + "\"");
 				SolidWorksDoc.DeleteConfiguration2(name);
 			}
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Delete Inactive Components", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Delete Inactive Components", IconName = "solidworks", Usage="Deletes assembly components that are suppressed in the active configuration")]
 	public class DeleteInactiveSwComponents : SolidworksAction
 	{
 		public DeleteInactiveSwComponents(IDictionary<String, Object> v, ICentipedeCore c)
@@ -369,9 +401,11 @@ namespace SolidworksActions
 			var components = (Component2[])assemblyDoc.GetComponents(true);
 			const int options = (int)swDeleteSelectionOptions_e.swDelete_Absorbed |
 								(int)swDeleteSelectionOptions_e.swDelete_Children;
+
 			foreach (var component in components.Where(component => (swComponentSuppressionState_e)component.GetSuppression() ==
 																	swComponentSuppressionState_e.swComponentSuppressed))
 			{
+                Message("Deleting inactive component: \"" + component.Name + "\"");
 				component.Select(false);
 				SolidWorksDoc.Extension.DeleteSelection2(options);
 			}
@@ -379,7 +413,7 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Save", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Save", IconName = "solidworks", Usage="Saves the active document and any referenced documents")]
 	public class SaveSolidWorks : SolidworksAction
 	{
 		public SaveSolidWorks(IDictionary<String, Object> v, ICentipedeCore c)
@@ -388,6 +422,8 @@ namespace SolidworksActions
 
 		protected override void DoAction()
 		{
+            Message("Saving file: \"" + SolidWorksDoc.GetTitle() + "\"");
+
 			const int options = (int)swSaveAsOptions_e.swSaveAsOptions_Silent |
 								(int)swSaveAsOptions_e.swSaveAsOptions_SaveReferenced;
 			int errs=0, warnings=0;
@@ -399,22 +435,24 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Save As", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Save As", IconName = "solidworks", Usage="Saves the active document as a new file, you can specify any supported file type")]
 	public class SolidWorksSaveAs : SolidworksAction
 	{
 		public SolidWorksSaveAs(IDictionary<String, Object> v, ICentipedeCore c)
 				: base("Save As", v, c)
 		{ }
 
-		[ActionArgument]
-		public String Filename = "";
+		[ActionArgument (Usage="Include the FileName extension, any file type conversion will be performed if necessary")]
+		public String FileName = "";
 
-		[ActionArgument(displayName = "Save as Copy")]
+		[ActionArgument(DisplayName = "Save as Copy")]
 		public bool SaveAsCopy;
 
 		protected override void DoAction()
 		{
-			string filename = Path.GetFullPath(Filename);
+            string myFileName = Path.GetFullPath(ParseStringForVariable(FileName));
+
+            Message("Saving file: \"" + SolidWorksDoc.GetTitle() + "\" as \"" + myFileName + "\"");
 
 			//tidy appearance
 			SolidWorksDoc.ViewDisplayShaded();
@@ -425,7 +463,7 @@ namespace SolidworksActions
 												? (int)swSaveAsOptions_e.swSaveAsOptions_Copy
 												: 0);
 
-			string path = Path.GetDirectoryName(filename);
+			string path = Path.GetDirectoryName(myFileName);
 			if (path != null && !Directory.Exists(path))
 			{
 				Directory.CreateDirectory(path);
@@ -433,7 +471,7 @@ namespace SolidworksActions
 
 
 			int errs = 0, warns = 0;
-			bool success = SolidWorksDoc.SaveAs4(filename, (int)swSaveAsVersion_e.swSaveAsCurrentVersion, options,
+			bool success = SolidWorksDoc.SaveAs4(myFileName, (int)swSaveAsVersion_e.swSaveAsCurrentVersion, options,
 												 ref errs, ref warns);
 			if (!success)
 			{
@@ -442,18 +480,20 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Close Active Document", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Close Active Document", IconName = "solidworks", Usage="Closes the currently active document")]
 	public class CloseActiveSwDoc : SolidworksAction
 	{
 		public CloseActiveSwDoc(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Close Active Document", v, c)
 		{ }
 
-		[ActionArgument(displayName = "Save Changes")]
+		[ActionArgument(DisplayName = "Save Changes")]
 		public bool SaveChanges;
 
 		protected override void DoAction()
 		{
+            Message("Closing file: \"" + SolidWorksDoc.GetTitle() + "\"");
+
 			if (SaveChanges)
 			{
 				const int options = (int)swSaveAsOptions_e.swSaveAsOptions_Silent |
@@ -478,20 +518,22 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Close All", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Close All", IconName = "solidworks", Usage="Closes all open documents")]
 	public class SwCloseAll : SolidworksAction
 	{
 		public SwCloseAll(IDictionary<String, Object> v, ICentipedeCore c)
 			: base("Close All", v, c)
 		{ }
 
-		[ActionArgument(displayName = "Save Changes")]
+		[ActionArgument(DisplayName = "Save Changes")]
 		public bool SaveChanges;
 
 		protected override void DoAction()
 		{
 			foreach (ModelDoc2 doc in SolidWorksWrapper.Instance.GetOpenDocuments())
 			{
+                Message("Closing file: \"" + doc.GetTitle() + "\"");
+
 				if (SaveChanges)
 				{
 					const int options = (int)swSaveAsOptions_e.swSaveAsOptions_Silent |
@@ -518,7 +560,7 @@ namespace SolidworksActions
 		}
 	}
 
-	[ActionCategory("SolidWorks", displayName = "Quit", iconName = "solidworks")]
+	[ActionCategory("SolidWorks", DisplayName = "Quit", IconName = "solidworks")]
 	public class Quit : SolidworksAction
 	{
 		public Quit(IDictionary<String, Object> v, ICentipedeCore c)
